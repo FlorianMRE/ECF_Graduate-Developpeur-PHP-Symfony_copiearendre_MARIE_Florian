ee524d88adfea676ed42b43fb5db66a666812fa2
FlorianMRE <sheixfx@gmail.com>
2023-05-16 15:59:20 +0200

    :sparkles: Création squelettes contact, sell, buy

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController extends AbstractController
+{
+    #[Route('/buy', name: 'app_products_buying')]
+    public function buying(): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'controller_name' => 'ProductsController',
+        ]);
+    }
+
+    #[Route('/sell', name: 'app_products_selling')]
+    public function selling(): Response
+    {
+        return $this->render('products/index.html.twig', [
+            'controller_name' => 'ProductsController',
+        ]);
+    }
+}
