1ce7757d09110a10218fd7f9f637b395280ee2b2
FlorianMRE <sheixfx@gmail.com>
2023-06-18 21:39:05 +0200

    :white_check_mark: Modify HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Opinion;
+use App\Form\OpinionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(
+        Request $request,
+        EntityManagerInterface $em,
+    ): Response
     {
-        return $this->render('home/adminProducts.html.twig', [
-            'controller_name' => 'HomeController',
+
+        $opinion = new Opinion();
+
+        $form = $this->createForm(OpinionType::class, $opinion);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $opinion->setCreatedAt(new \DateTimeImmutable());
+
+            $em->persist($opinion);
+            $em->flush();
+
+            unset($opinion);
+            unset($form);
+            $opinion = new Opinion();
+            $form = $this->createForm(OpinionType::class, $opinion);
+
+            $this->redirectToRoute('app_home');
+        }
+
+        $opinions = $em->getRepository(Opinion::class)->getOpinionsPublished();
+
+        return $this->render('home/home.html.twig', [
+            'formOpinion' => $form->createView(),
+            'opinions' => $opinions
         ]);
     }
 }
