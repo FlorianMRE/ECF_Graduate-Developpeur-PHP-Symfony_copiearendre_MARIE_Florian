fbfc1b6c5f2f51c2840ce78a07426dfb5d4596bb
FlorianMRE <sheixfx@gmail.com>
2023-06-18 21:40:06 +0200

    :white_check_mark: GarageServices (Prestations)

--- src/Controller/GarageServicesController.php
+++ src/Controller/GarageServicesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Prestations;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class GarageServicesController extends AbstractController
 {
     #[Route('/services', name: 'app_services')]
-    public function index(): Response
+    public function index(
+        EntityManagerInterface $em
+    ): Response
     {
-        return $this->render('garage_services/index.html.twig', [
-            'controller_name' => 'GarageServicesController',
+        $allPrestations = $em->getRepository(Prestations::class)->findAll();
+
+        return $this->render('garage_services/garageServices.html.twig', [
+            'allPrestations' => $allPrestations,
         ]);
     }
 }
