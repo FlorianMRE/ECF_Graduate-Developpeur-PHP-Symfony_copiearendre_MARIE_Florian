35bfcf3a3d37af778d9e5dd7f82a40bb21bbf8ba
FlorianMRE <sheixfx@gmail.com>
2023-07-04 13:18:24 +0200

    :white_check_mark: Remove error div register form

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -75,7 +75,6 @@ class RegisterType extends AbstractType
                 'invalid_message' => 'Les mots de passe doivent être identiques',
                 'first_options'  => ['label' => 'Password', 'attr' => ['placeholder' => 'Entrez votre mot de passe']],
                 'second_options' => ['label' => 'Vérification du mot de passe', 'attr' => ['placeholder' => 'Entrez de nouveau votre mot de passe']],
-                'error_bubbling' => true
             ])
         ;
         $builder->get('roles')
