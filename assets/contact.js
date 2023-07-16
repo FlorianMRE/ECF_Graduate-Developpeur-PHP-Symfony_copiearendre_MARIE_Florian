70b694f04f931dbc9fa72fa56af072911393752a
FlorianMRE <sheixfx@gmail.com>
2023-07-16 19:15:55 +0200

    :white_check_mark: edit openingHours js problem toggle none

--- assets/contact.js
+++ assets/contact.js
@@ -1,8 +1,14 @@
 input = document.querySelector('#hours_close_day')
 
+if (input.checked) {
+    document.querySelectorAll('.hours-item-container').forEach((element) => {
+        element.classList.add('none')
+    })
+}
+
 input.addEventListener('change', (e) => {
 
-    document.querySelectorAll('.hours-item').forEach((element) => {
+    document.querySelectorAll('.hours-item-container').forEach((element) => {
         element.classList.toggle('none')
     })
 
