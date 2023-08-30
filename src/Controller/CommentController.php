454f691dbf9ce3774e0413bfab29fd0b427c87b9
FlorianMRE <sheixfx@gmail.com>
2023-08-30 13:46:56 +0200

    :white_check_mark: delete comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comments;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,6 +21,20 @@ class CommentController extends AbstractController
 
         $em->persist($comment);
         $em->flush();
+
+        return $this->redirectToRoute('app_product_buy_show', ['slug' => $comment->getProduct()->getSlug()]);
+    }
+
+    #[Route('/comment/delete/{id}', name: 'app_comment_delete')]
+    public function delete(
+        Comments $comment,
+        EntityManagerInterface $em,
+        Request $request
+    ): Response
+    {
+        $em->getRepository(Comments::class)->remove($comment, true);
+
+        $this->addFlash('error', 'Commentaire supprimer');
 
         return $this->redirectToRoute('app_product_buy_show', ['slug' => $comment->getProduct()->getSlug()]);
     }
